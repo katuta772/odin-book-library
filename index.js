69c24db6e0a74916b2212f44c2ced5d3f30a14c1
katuta772 <gogan.catalin@rocketmail.com>
2025-01-25 22:48:58 +0200

    added next/prev buttons functionality, organised the files

--- index.js
+++ index.js
@@ -1,55 +1,77 @@
 const myLibrary = [];
-const content = document.querySelector('.content')
+const content = document.querySelector(".content");
 
+var contentPage = document.getElementById("page").textContent;
 
-function Book(name,author,pages,status){
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+function Book(name, author, pages, status) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
 
-function addBookToLibrary(name, author, pages, status){
-    const book = new Book(name,author,pages,status);
-    myLibrary.push(book);
+function addBookToLibrary(name, author, pages, status) {
+  const book = new Book(name, author, pages, status);
+  myLibrary.push(book);
 }
 
-function createCard(book){
-    const card = document.createElement('div');
-    card.classList.add('card');
+function createCard(book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-    const cardName = document.createElement('div');
-    const cardAuthor = document.createElement('div');
-    const cardPages = document.createElement('div');
-    const cardStatus = document.createElement('div');
+  const cardName = document.createElement("div");
+  const cardAuthor = document.createElement("div");
+  const cardPages = document.createElement("div");
+  const cardStatus = document.createElement("div");
 
-    cardName.textContent = book.name;
-    cardName.id = 'name';
-    cardAuthor.textContent = book.author;
-    cardAuthor.id = 'author';
-    cardPages.textContent = book.pages + ' pages';
-    cardPages.id = 'pages';
-    cardStatus.textContent = book.status;
-    cardStatus.id = 'status';
-    
-    card.appendChild(cardName);
-    card.appendChild(cardAuthor);
-    card.appendChild(cardPages);
-    card.appendChild(cardStatus);
+  cardName.textContent = book.name;
+  cardName.id = "name";
+  cardAuthor.textContent = book.author;
+  cardAuthor.id = "author";
+  cardPages.textContent = book.pages + " pages";
+  cardPages.id = "pages";
+  cardStatus.textContent = book.status;
+  cardStatus.id = "status";
 
-    content.appendChild(card);
+  card.appendChild(cardName);
+  card.appendChild(cardAuthor);
+  card.appendChild(cardPages);
+  card.appendChild(cardStatus);
+
+  content.appendChild(card);
 }
 
-function showBooks(array){
-    array.forEach(element => {
-        createCard(element);
-    });
+function nextPage() {
+  if (contentPage * 4 < myLibrary.length) contentPage++;
+  document.getElementById("page").textContent = contentPage;
+  content.innerHTML = '';
+  showBooks();
+}
+
+function prevPage() {
+  if (contentPage == 1) {
+  } else {
+    contentPage--;
+    document.getElementById("page").textContent = contentPage;
+    content.innerHTML = '';
+    showBooks();
+  }
+}
+
+function showBooks() {
+    for(index = (contentPage * 4 - 4); index <(contentPage * 4); index++){
+        createCard(myLibrary[index])
+    }
 }
 
 // Test examples
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien",295, 'Not Read');
-addBookToLibrary("The Hobbit2", "J.R.R. Tolkien",295, 'Not Read');
-addBookToLibrary("The Hobbit3", "J.R.R. Tolkien",295, 'Not Read');
-addBookToLibrary("The Hobbit4", "J.R.R. Tolkien",295, 'Not Read');
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "Not Read");
+addBookToLibrary("The Hobbit2", "J.R.R. Tolkien", 295, "Not Read");
+addBookToLibrary("The Hobbit3", "J.R.R. Tolkien", 295, "Not Read");
+addBookToLibrary("The Hobbit4", "J.R.R. Tolkien", 295, "Not Read");
+addBookToLibrary("The Hobbit5", "J.R.R. Tolkien", 295, "Not Read");
+addBookToLibrary("The Hobbit6", "J.R.R. Tolkien", 295, "Not Read");
+addBookToLibrary("The Hobbit7", "J.R.R. Tolkien", 295, "Not Read");
+addBookToLibrary("The Hobbit8", "J.R.R. Tolkien", 295, "Not Read");
 
-showBooks(myLibrary);
+showBooks();
